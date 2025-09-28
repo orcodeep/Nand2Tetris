31e4a8a870b21ce277cf00272c3f29fa3008f51e
sam_lappy <a@g.com>
2025-09-29 01:09:58 +0530

    working on mult.asm

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @i 
+    M = 0
+
+(CHECK)
+    @i 
+    D = M 
+    @R1 
+    D = D - M 
+    @LOOP
+    D; JLT 
+
+(LOOP)
+    @R0 
+    D = M
+    M = D + M 
+    @i 
+    M = M + 1
+    @CHECK 
+    0; JMP 
+
+
+// store mult value in RAM[2]
+    @R0 
+    D = M
+    @R2 
+    M = D
+
+(END)
+    @END
+    0; JMP 
