ea65b4da3dcdef5233c59683be56f848e79af857
sam_lappy <a@g.com>
2025-09-29 21:22:17 +0530

    working on fill.asm

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,73 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@SCREEN 
+D = A 
+@i 
+A = D 
+
+// store the last register's address + 1. which is = RAM[l]
+@l 
+M = D 
+@8192 
+D = A 
+@l 
+M = M + D
+
+@KBD 
+D = M 
+@FILL
+D; JNE 
+
+@SCREEN 
+D = A 
+@i 
+A = D 
+@CLEAR 
+0; JMP 
+
+(FILL)
+    @i 
+    D = A
+    @l 
+    D = D - M 
+    @FLOOP
+    D; JLT 
+    @SCREEN 
+    D = A 
+    @i 
+    A = D
+
+(FLOOP) 
+    @i 
+    M = 1
+    @16 
+    D = A 
+    @i 
+    A = D + A 
+    @FILL 
+    0; JMP 
+
+(CLEAR)
+    @i 
+    D = A
+    @l 
+    D = D - M 
+    @CLOOP
+    D; JLT 
+    @END 
+    0; JMP 
+
+(CLOOP)
+    @i 
+    M = 1
+    @16 
+    D = A 
+    @i 
+    A = D + A 
+    @CLEAR  
+    0; JMP 
+
+(END) 
+    @END 
+    0; JMP 
