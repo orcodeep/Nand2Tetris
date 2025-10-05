a1f81a00ce9b4e351d76a0cf2de51f284ce0b038
sam_pc <a@g.com>
2025-10-05 05:31:15 -0700

    .

--- 6/parser.c
+++ 6/parser.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-typedef struct line {
-    char* buffer;
-}line;
+
 
 typedef struct lineNode {
 
-    line* line;
+    char* line;
     int n; // linenumber
     struct lineNode* next;
 } lineNode; 
@@ -26,35 +24,59 @@ int fileopen(char* filename)
     if(fileptr == NULL)
         return 1;
     
-    size_t size = 2 * sizeof(char); 
-    char* buffer = malloc(size);
+    int initial_size = 4 * sizeof(char);
+    size_t size = initial_size; // this is the min. ex:- @R10
+    char* line = malloc(size);
+    lineNode* linenode = malloc(sizeof(lineNode));
+    linenode->line = line; // point the first linenode to this first line buffer.
+    linenode->n = 1;
+    linenode->next = NULL;
 
     int c;
     char ch;
     int i = 1;
-    int buffer_index;
+    int buffer_index = 0;
+    lineNode* current_linenode = linenode;
     while((c = fgetc(fileptr)) != EOF)
     {
-        
-        if (i > size)
+        // check if end of a line end reached i.e '\n' (newline character) encountered then end of line
+        if (c == '\n')
         {
-            char* temp = realloc(buffer, i*sizeof(char));
+            line[buffer_index] = '\0'; // make it a c string
+
+            i = 1;
+            size = initial_size;
+            line = malloc(size);
+
+            lineNode* new_linenode = malloc(sizeof(lineNode));
+            current_linenode->next = new_linenode;
+            (new_linenode)->line = line;
+            new_linenode->n = current_linenode->n + 1;
+            new_linenode->next = NULL;
+            current_linenode = new_linenode;
+            
+            buffer_index = 0;
+            continue;
+        }
+        
+        // check if line size > buffer size
+        if (i*sizeof(char) > size)
+        {
+            size *= 2;
+            char* temp = realloc(line, size); // 2x the size of buffer
             if (temp == NULL)
             {
-                free(buffer);
+                free(line);
                 return 2;
             }
-            buffer = temp;
+
+            line = temp;
         }
 
 
-
+        i++;
         buffer_index = i-1;
         ch = (char) c; 
-        buffer[buffer_index] = ch;
-
-
-
-        i++;
+        line[buffer_index] = ch;
     }
 }
