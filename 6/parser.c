bb547ffc2c2f80dba30336bc53539a515b806063
sam_lappy <a@g.com>
2025-10-05 16:41:36 +0530

    .

--- 6/parser.c
+++ 6/parser.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
+typedef struct line {
+    char* buffer;
+}line;
+
+typedef struct lineNode {
+
+    line* line;
+    int n; // linenumber
+    struct lineNode* next;
+} lineNode; 
+
 
 int fileopen(char* filename);
 
 
 
-// first open the .asm file 
+
+
+// first open the .asm file and read and store in linked list then return ptr to 
+// this linked list
 int fileopen(char* filename)
 {
-    FILE* ptr = fopen(filename, "r");
+    FILE* fileptr = fopen(filename, "r");
+    if(fileptr == NULL)
+        return 1;
+    
+    char* buffer = malloc(2 * sizeof(char));
+
+    int c;
+    char ch;
+    while((c = fgetc(fileptr)) != EOF)
+    {
+        ch = (char) c;
+    }
 }
