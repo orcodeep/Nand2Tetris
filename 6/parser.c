d20445fa6ad2a5cc433c7398ab4c739be56ae269
sam_lappy <a@g.com>
2025-10-05 22:17:26 +0530

    .

--- 6/parser.c
+++ 6/parser.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
 
@@ -27,7 +28,16 @@ int fileopen(char* filename)
     int initial_size = 4;
     size_t size = initial_size; // this is the min. ex:- @R10
     char* line = malloc(size*sizeof(char));
+    if (line == NULL)
+    {
+        return 2;
+    }
     lineNode* linenode = malloc(sizeof(lineNode));
+    if (linenode == NULL)
+    {
+        free(line);
+        return 2;
+    }
     linenode->line = line; // point the first linenode to this first line buffer.
     linenode->next = NULL;
 
@@ -37,6 +47,7 @@ int fileopen(char* filename)
     int line_number = 0;
     int buffer_index = 0;
     lineNode* current_linenode = linenode;
+    bool comment_text = false;
     while((c = fgetc(fileptr)) != EOF)
     {
 
@@ -54,13 +65,22 @@ int fileopen(char* filename)
             }
 
             line = temp;
+            current_linenode->line = line;
         }
-
-
+        if (c == '/')
+        {
+            comment_text = true;
+        }
+ 
+        if (c == ' ' || c == '\t' || (c != '\n' && comment_text))
+        {
+            continue;
+        }    
         // check if end of a line end reached i.e '\n' (newline character) encountered 
         // then end of line. now make new node for next line and reset all counters.
-        if (c == '\n')
+        else if (c == '\n')
         {
+            comment_text = false;
             // if no character before it 
             if (buffer_index == 0)
             {
@@ -75,31 +95,40 @@ int fileopen(char* filename)
                 char_number = 1;
                 buffer_index = 0;
                 size = initial_size;
-                line = malloc(size);
-    
+                char* tmp = realloc(line, size*sizeof(char));
+                if (tmp == NULL)
+                {
+                    return 2;
+                }
+                line = tmp;
+
                 lineNode* new_linenode = malloc(sizeof(lineNode));
+                if (new_linenode == NULL)
+                {
+                    free(line);
+                    lineNode* freeptr = linenode;
+                    while(freeptr != NULL)
+                    {
+                        linenode = freeptr->next;
+                        free(freeptr->line);
+                        free(freeptr);
+                        freeptr = linenode;
+                    }
+                    return 2;
+                }
+                new_linenode->next = NULL;
                 current_linenode->next = new_linenode;
                 new_linenode->line = line;
-                new_linenode->next = NULL;
                 current_linenode = new_linenode;
                 continue;
             }
         } 
-        else if (c == ' ' || c == '\t')
-        {
-            continue;
-        }    
-        else if (c == '/')
-        {
-
-        }
-        
         else
         {
-            char_number++;
-            buffer_index++;
             // ch = (char) c; 
             line[buffer_index] = (char) c;
+            char_number++;
+            buffer_index++;
         }
     }
 
